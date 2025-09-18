ce10fd2105f176955410d4c33afcf9bd5f6542dd
xue1866 <3505193584@qq.com>
2025-09-18 19:59:51 +0800

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -7,7 +7,10 @@ router.get("/login", async function (req, res, next) {
   console.log("登录", req.query);
   
   console.log("9行");
-  let user = await usersModel.findOne(req.query);
+  let user = await usersModel.findOne({
+    user: req.query.user,
+    pwd: req.query.pwd
+  });
   console.log("11行");
   console.log("user:",user);
   
