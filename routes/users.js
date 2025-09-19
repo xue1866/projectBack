d1a6e256d6d4b1d0ab59a0f799fe677eabb502f3
xue1866 <3505193584@qq.com>
2025-09-19 11:46:05 +0800

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -4,31 +4,11 @@ const router = express.Router();
 
 // /* 登录 */
 router.get("/login", async function (req, res, next) {
-  console.log("登录", req.query);
-  
-  console.log("user:",typeof req.query.user);
-  console.log("pwd:",typeof  req.query.pwd);
-  console.log("9行");
-  console.log("数据库字段:", Object.keys(usersModel.schema.obj));
-  console.log("数据库字段:",typeof (Object.keys(usersModel.schema.obj)[0]));
   let user = await usersModel.findOne({
     user: req.query.user,
     pwd: req.query.pwd
   });
-  let user1 = await usersModel.findOne({
-    user: "admin",
-    pwd: "admin"
-  });
-  let user2 = await usersModel.findOne({
-    "user": "admin",
-    "pwd": "admin"
-  });
-  let user3 = await usersModel.find();
   console.log("user:",user);
-  console.log("user1:",user1);
-  console.log("user1:",user2);
-  console.log("user1:",user3);
-  
   if (user) {
     res.send({
       code: 200,
